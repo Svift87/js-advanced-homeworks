11e806333974197c2616287712a1cc9ea986bff3
Sergey <kuryakov-s@mail.ru>
2019-01-23 20:42:40 +0700

    Первое задание №2

--- standarts/js/js.js
+++ standarts/js/js.js
@@ -9,11 +9,16 @@ const calculateBonus = (starts, percent, prices) => {
     return Math.round(bonusStart * percent / 100);
 }
 
-console.log(`Бонус составил ${calculateBonus(10000, 5, [200, 550, 4000, 23, 58, 5000, 485, 711])} игровых баллов!`);
+let prices = [200, 550, 4000, 23, 58, 5000, 485, 711];
+let starts = 10000;
+let percent = 5;
+
+console.log(`Бонус составил ${calculateBonus(starts, percent, prices)} игровых баллов!`);
 
 // Задание 2
 
-const showBalance = (value) => {
+const showBalance = (string, value) => {
+    this.string = string;
     let result;
     if (Number(String(value).slice(-1)) === 0 || Number(String(value).slice(-1)) > 4) {
         result = " баллов"
@@ -22,46 +27,29 @@ const showBalance = (value) => {
     } else {
         result = " балла"
     }
-    const string = "Ваш баланс: "
-    return `${string}${value}${result}`
+    return `${string[0]}${value}${result}`
 }
-
-console.log(showBalance(523));
-console.log(showBalance(6000));
-console.log(showBalance(5001));
+const value = 1;
+const output = showBalance`Ваш баланс: ${value}`;
+console.log(output);
 
 // Задание 3
 
-function _toConsumableArray(arr) { 
-    if (Array.isArray(arr)) { 
-        for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { 
-        arr2[i] = arr[i]; 
-    } 
-    return arr2; 
-    } else { 
-        return Array.from(arr); 
-    } 
-}
+let pointsArray = [74989, 74990, 74990, 62000, 58480, 61800];
 
-function findTop(value) {
-    var sort = Math.max.apply(Math, _toConsumableArray(value));
-    return sort;
-}
+let pointsArraySorted = pointsArray.sort(function cmpNum(a, b) {
+  return b - a;
+});
 
-console.log("Самый лучший балл: " + findTop([74989, 74990, 74990, 62000, 58480, 61800]) + "!");
+let winnerPoints = pointsArraySorted[0];
 
+let medallistAverage =
+  pointsArraySorted.slice(0, 3).reduce(function sum(a, b) {
+    return a + b;
+  }) / 3;
 
-function findTop3(value) {
-    function sortArray(a, b) {
-        return a - b;
-    }
-    var top3Array = value.sort(sortArray).splice(-3);
-    var sum = 0;
-    for (var i = 0; i < top3Array.length; i++) {
-        sum += top3Array[i];
-    }
-    return sum / 3;
-}
+medallistAverage = medallistAverage.toFixed(2);
 
-console.log("Средний балл из топ-3: " + Math.round(findTop3([74989, 74990, 74990, 62000, 58480, 61800])) + "!");
+console.log("Наивысший балл: " + winnerPoints);
+console.log("Средний балл призёров: " + medallistAverage);
 
